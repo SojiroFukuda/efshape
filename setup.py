8042c663d526818a7ccbed1b74a6510d58cf83ac
SojiroFukuda <50654185+SojiroFukuda@users.noreply.github.com>
2019-06-27 22:08:07 +0100

    igloos

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fps_pca_gui_analyzer",
+    version="0.0.1",
+    description='package for bulding the GUI software for grain shape analysis based on elliptic Fourier analysis',
+    author='Sojiro Fukuda',
+    author_email='s.fukuda-2018@hull.ac.uk',
+    url="https://github.com/SojiroFukuda/FPS-PCA-GUI",
+
+    install_requires=["numpy", "PyQt5","sys","matplotlib","os","re","glob","pandas","random","pathlib","datatime","cv2","scipy","sklearn"],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
