c3f88dd972dd25c2558f4adf97b711e07a502fdb
SojiroFukuda <50654185+SojiroFukuda@users.noreply.github.com>
2019-06-27 22:26:23 +0100

    a

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name="fps_pca_gui_analyzer",
       author='Sojiro Fukuda',
       author_email='s.fukuda-2018@hull.ac.uk',
       url="https://github.com/SojiroFukuda/FPS-PCA-GUI",
-      packages=['distutils', 'distutils.command'],
+      packages=["numpy", "PyQt5","sys","matplotlib","os","re","glob","pandas","random","pathlib","datatime","cv2","scipy","sklearn"],
      )
