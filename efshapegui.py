16e95e93c10768b38cfb95d1fc42df521f0f46bd
SojiroFukuda <50654185+SojiroFukuda@users.noreply.github.com>
2019-07-14 17:04:45 +0100

    Update efshapegui.py

--- efshapegui.py
+++ efshapegui.py
@@ -1,14 +1,18 @@
 from PyQt5 import QtWidgets as Qw   
 import guigenerator as gui
 import sys
-
+def startGUI():
+    app = Qw.QApplication(sys.argv)         
+    wmain = gui.MyForm()                        
+    wmain.show()                            
+    sys.exit(app.exec_())
 ####----------------------------------------
 ####   Execute this file to open GUI
 ####----------------------------------------
 #   main
 if __name__ == '__main__':
-
-    app = Qw.QApplication(sys.argv)         
-    wmain = gui.MyForm()                        
-    wmain.show()                            
-    sys.exit(app.exec_())
+    startGUI()
+#     app = Qw.QApplication(sys.argv)         
+#     wmain = gui.MyForm()                        
+#     wmain.show()                            
+#     sys.exit(app.exec_())
