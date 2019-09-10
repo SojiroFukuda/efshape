74ec4e666662a0ccc9422c2ec5b7d6b6f0fbc176
SojiroFukuda <50654185+SojiroFukuda@users.noreply.github.com>
2019-09-10 16:17:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     version="1.1.0",
     packages=find_packages(),
     install_requires=["PyQt5", "matplotlib","numpy","pandas","scipy","sklearn"],
+    extras_require={
+#         "develop": ["dev-packageA", "dev-packageB"]
+    },
     author="Sojiro Fukuda",
     author_email="S.Fukuda-2018@hull.ac.uk",
     entry_points={
-        "console_scripts": [
-            "efshape = startGUI"
-        ],
-        "gui_scripts": [
-            "efshapegui = startGUI"
-        ]
+        "console_scripts":
+            "efshapeGUI = efshapegui:main"
     }
 )
