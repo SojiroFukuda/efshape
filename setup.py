00d4cb3bf8f4838f99214eb7abd87597c2d6c57f
SojiroFukuda <50654185+SojiroFukuda@users.noreply.github.com>
2019-09-10 13:57:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="efshapegui",
+    version="1.1.0)",
+    install_requires=["PyQt5", "matplotlib","numpy","pandas","scipy","sklearn"],
+    extras_require={
+#         "develop": ["dev-packageA", "dev-packageB"]
+    },
+    entry_points={
+        "console_scripts": [
+            "efshape = efshapegui:startGUI"
+        ],
+        "gui_scripts": [
+            "efshapeGUI = efshapegui:startGUI"
+        ]
+    }
+)
