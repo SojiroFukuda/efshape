c4d1a2874b7914d835bad5e25db328ef2d6b0382
SojiroFukuda <50654185+SojiroFukuda@users.noreply.github.com>
2019-09-10 15:43:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version="1.1.0",
     packages=find_packages(),
     install_requires=["PyQt5", "matplotlib","numpy","pandas","scipy","sklearn"],
-    extras_require={
-#         "develop": ["dev-packageA", "dev-packageB"]
-    },
     author="Sojiro Fukuda",
     author_email="S.Fukuda-2018@hull.ac.uk",
     entry_points={
